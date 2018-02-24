71467a8414757b7209bf3aef5006049ba63a701f
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-02-24 14:23:17 +0100

    update api

--- js/api.js
+++ js/api.js
@@ -1,10 +1,8 @@
 export const getItems = lastUpdate => {
-	return;
+	
 };
 
-export const getCategories = lastUpdate => {
-	return apiClient.get("update/categories/0").then(res => {
+export const getCategories = lastUpdate => apiClient.get("update/categories/0").then(res => {
 		const categoryList = res.data.nav;
 		return categoryList;
 	});
-};
