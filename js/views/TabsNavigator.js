b01932dab67f61a83b1cb2b4af2789e744890254
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-03-02 11:42:41 +0100

    style tabs

--- js/views/TabsNavigator.js
+++ js/views/TabsNavigator.js
@@ -1,12 +1,22 @@
+// @flow
 import React from "react";
 import { TabNavigator } from "react-navigation";
-import CategoriesStack from "./CategoriesStack";
 import { Icon } from "react-native-elements";
+import styled from "styled-components";
+import CategoriesStack from "./CategoriesStack";
 
 const EAT_CATEGORIES_ID = 30;
 const SLEEP_CATEGORIES_ID = 1;
 const TODO_CATEGORIES_ID = 36;
 const SERVICES_CATEGORIES_ID = 46;
+
+const ACTIVE_TINT_COLOR = "#0a71b3";
+
+const TabIcon = styled(Icon).attrs({
+  color: props =>
+    props.focused ? props.theme.colors.primary : props.inactiveTintColor,
+  size: 25
+})``;
 
 export default TabNavigator(
   {
@@ -19,41 +29,52 @@ export default TabNavigator(
     backBehavior: "none",
     swipeEnabled: false,
     animationEnabled: false,
+    lazy: false,
+    tabBarOptions: {
+      activeTintColor: ACTIVE_TINT_COLOR
+    },
     navigationOptions: ({ navigation }) => ({
       tabBarPosition: "bottom",
       tabBarIcon: ({ focused, tintColor }) => {
         const { routeName } = navigation.state;
-        let iconName;
         if (routeName === "EatTab") {
           return (
-            <Icon name="food" type="material-community" size={25} color="red" />
+            <TabIcon
+              focused={focused}
+              name="food"
+              type="material-community"
+              inactiveTintColor={tintColor}
+            />
           );
         } else if (routeName === "SleepTab") {
           return (
-            <Icon
+            <TabIcon
+              focused={focused}
               name="hotel"
               type="material-community"
-              size={25}
-              color="red"
+              inactiveTintColor={tintColor}
             />
           );
         } else if (routeName === "ServicesTab") {
           return (
-            <Icon
+            <TabIcon
+              focused={focused}
               name="marker"
               type="material-community"
-              size={25}
-              color="red"
+              inactiveTintColor={tintColor}
             />
           );
         } else if (routeName === "TodoTab") {
           return (
-            <Icon name="mountains" type="foundation" size={25} color="red" />
+            <TabIcon
+              focused={focused}
+              name="mountains"
+              type="foundation"
+              inactiveTintColor={tintColor}
+            />
           );
         }
-
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
+        return null;
       }
     })
   }
