312ae36f3d728ac93813b11087886405ec4f2b6c
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-03-07 00:04:51 +0100

    add realm

--- android/app/src/main/java/com/guiaanimus/MainApplication.java
+++ android/app/src/main/java/com/guiaanimus/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new RNDeviceInfo(),
             new ReactNativePushNotificationPackage(),
             new RCTMGLPackage(),
