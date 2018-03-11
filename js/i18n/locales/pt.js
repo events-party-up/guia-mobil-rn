4c3511429b0230ad3a85bb497e0d4da64e39770e
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-03-12 00:24:47 +0100

    add portuguese translation asset

---
+++ js/i18n/locales/pt.js
@@ -0,0 +1,11 @@
+export default {
+  discoverPlace: "Descubrí Bariloche",
+  activitiesHeadline: "Las mejores actividades para disfrutar.",
+  activitiesHeadlineSub:
+    "Conocé los lugares que recomendamos para visitar y disfrutar.",
+  navigation: {
+    settings: {
+      title: "Configuración"
+    }
+  }
+};
